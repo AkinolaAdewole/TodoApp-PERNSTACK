041b1200eeffe0ca5aedb65d177f1baaf390b7d1
Akinola <adewoleakinola24@gmail.com>
2023-07-24 19:11:18 +0100

    client side

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React,{Fragment} from 'react';
 import './App.css';
 import InputTodo from './components/InputTodo';
+import DisplayTodo from './components/DisplayTodo';
 
 function App() {
   return <Fragment>
         <div className="container">
           <InputTodo/>
+          <DisplayTodo/>
         </div>
            
         </Fragment>
---
+++ frontend/src/components/DisplayTodo.jsx
@@ -0,0 +1,65 @@
+import React,{Fragment, useState, useEffect} from 'react'
+
+const DisplayTodo = () => {
+    const getTodos= async ()=>{
+        try {
+        } catch(err){
+            console.error(err);
+        }
+    }
+
+    useEffect(()=>{
+        getTodos()
+    })
+  return (
+    <div>
+        <Fragment>
+        <table class="table mt-5 text-center">
+    <thead>
+      <tr>
+        <th>Description</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* <tr>
+        <td>Default</td>
+        <td>Defaultson</td>
+        <td>def@somemail.com</td>
+      </tr>      
+      <tr class="success">
+        <td>Success</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr class="danger">
+        <td>Danger</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr class="info">
+        <td>Info</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+      <tr class="warning">
+        <td>Warning</td>
+        <td>Refs</td>
+        <td>bo@example.com</td>
+      </tr>
+      <tr class="active">
+        <td>Active</td>
+        <td>Activeson</td>
+        <td>act@example.com</td>
+      </tr> */}
+
+
+    </tbody>
+  </table>
+        </Fragment>
+    </div>
+  )
+}
+
+export default DisplayTodo
