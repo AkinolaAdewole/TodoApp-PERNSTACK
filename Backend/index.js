29207dcc48727bb364827c8b36ee308b099b5991
Akinola <adewoleakinola24@gmail.com>
2023-07-24 15:30:01 +0100

    Add PostgreSQL

--- Backend/index.js
+++ Backend/index.js
@@ -4,12 +4,30 @@ const cors= require('cors');
 
 const pool= require('./db');
 
-
-
-
 // Middleware
 App.use(cors());
 App.use(express.json());
+
+// ROUTES
+
+// Create a todo
+App.post("/todos", async(req,res)=>{
+    try {
+        // console.log(req.body);
+        const {description} = req.body;
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1)")
+    } catch (err){
+        console.log(err.message);
+    }
+});
+
+//get all todo
+
+// get a todo
+
+// update a todo
+
+// delete a todo
 
 App.listen(5000, ()=>{
     console.log("server has started on port 5000");
