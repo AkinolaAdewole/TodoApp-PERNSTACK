a3405ec9743dee2e7620f356cd0e4d025e8a2950
Akinola <adewoleakinola24@gmail.com>
2023-07-24 17:51:57 +0100

    client side

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Fragment} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <Fragment>
+    
+  </Fragment>
 }
 
 export default App;
