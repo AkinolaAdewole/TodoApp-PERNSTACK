740ddc048efd17a864ffe0ebae0d5195306714d5
Akinola <adewoleakinola24@gmail.com>
2023-07-24 18:20:52 +0100

    client side

--- frontend/src/components/InputTodo.jsx
+++ frontend/src/components/InputTodo.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React,{Fragment, useState} from 'react'
 
 const InputTodo = () => {
+   const [description, setDescription]= useState("")
   return (
-    <div></div>
+    <Fragment>
+        <h1 className='text-center mt-5'>TODO</h1>
+        <form className='d-flex mt-5' >
+            <input type="text" className='form-control' value={description}
+             onChange={(e)=>setDescription(e.target.value)}/>
+            <button className='btn btn-success'> Add Todo</button>
+        </form>
+    </Fragment>
   )
 }
 
-export default InputTodo
+export default InputTodo 
