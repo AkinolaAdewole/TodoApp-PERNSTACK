c92306a4d57e6e9ef7505dc12c52ae75c0a0c2c7
Akinola <adewoleakinola24@gmail.com>
2023-07-24 20:03:52 +0100

    Edit Todo

--- frontend/src/components/DisplayTodo.jsx
+++ frontend/src/components/DisplayTodo.jsx
@@ -1,4 +1,6 @@
-import React,{Fragment, useState, useEffect} from 'react'
+import React,{Fragment, useState, useEffect} from 'react';
+import EditTodo from './EditTodo';
+
 
 const DisplayTodo = () => {
 
@@ -45,39 +47,19 @@ const DisplayTodo = () => {
       </tr>
     </thead>
     <tbody>
-      {/* <tr>
-        <td>Default</td>
-        <td>Defaultson</td>
-        <td>def@somemail.com</td>
-      </tr>      
-      <tr class="success">
-        <td>Success</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr class="danger">
-        <td>Danger</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr class="info">
-        <td>Info</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-      */}
-      {todos.map((todo)=>(
-        <tr key={todo.todo_id}>
-            <td>{todo.description}</td>
-            <td>Edit</td>
+     
+        {todos.map((todo)=>(
+            <tr key={todo.todo_id}>
+                <td>{todo.description}</td>
+                <td> <EditTodo /> </td>
 
-            <td>
-                <button 
-                    className='btn btn-danger'
-                    onClick={()=>deleteTodo(todo.todo_id)}>Delete</button>
-            </td>
-        </tr>
-      ))}
+                <td>
+                    <button 
+                        className='btn btn-danger'
+                        onClick={()=>deleteTodo(todo.todo_id)}>Delete</button>
+                </td>
+            </tr>
+        ))}
 
 
     </tbody>
--- frontend/src/components/EditTodo.jsx
+++ frontend/src/components/EditTodo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const EditTodo = () => {
   return (
-    <div>EditTodo</div>
+    <div>Edit Todo</div>
   )
 }
 
